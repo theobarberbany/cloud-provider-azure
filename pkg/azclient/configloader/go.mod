d8071ee85d222092f130d04ae96125bc6f1be34d
Damiano Donati <damiano.donati@gmail.com>
2023-08-23 12:05:27 +0200

    UPSTREAM: <carry>: Updating and vendoring go modules after an upstream rebase

--- pkg/azclient/configloader/go.mod
+++ pkg/azclient/configloader/go.mod
@@ -14,7 +14,7 @@ require (
 )
 
 require (
-	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.6.1 // indirect
+	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.7.0 // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/azidentity v1.3.0 // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/internal v1.3.0 // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/resourcemanager/compute/armcompute/v5 v5.0.0 // indirect
@@ -53,12 +53,12 @@ require (
 	go.opentelemetry.io/otel v1.16.0 // indirect
 	go.opentelemetry.io/otel/metric v1.16.0 // indirect
 	go.opentelemetry.io/otel/trace v1.16.0 // indirect
-	golang.org/x/crypto v0.10.0 // indirect
+	golang.org/x/crypto v0.11.0 // indirect
 	golang.org/x/net v0.11.0 // indirect
 	golang.org/x/oauth2 v0.9.0 // indirect
-	golang.org/x/sys v0.9.0 // indirect
-	golang.org/x/term v0.9.0 // indirect
-	golang.org/x/text v0.10.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	golang.org/x/term v0.10.0 // indirect
+	golang.org/x/text v0.11.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
 	golang.org/x/tools v0.9.3 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
---
+++ pkg/azclient/configloader/vendor/sigs.k8s.io/cloud-provider-azure/pkg/azclient/utils/options.go
@@ -0,0 +1,59 @@
+/*
+Copyright 2023 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package utils
+
+import (
+	"math"
+	"net/http"
+	"time"
+
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/arm"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/policy"
+	"github.com/Azure/azure-sdk-for-go/sdk/azcore/tracing"
+
+	"sigs.k8s.io/cloud-provider-azure/pkg/azclient/policy/retryrepectthrottled"
+)
+
+const (
+	DefaultMaxRetries    = 3
+	DefaultMaxRetryDelay = math.MaxInt64
+	DefaultRetryDelay    = 5 * time.Second
+	DefaultTryTimeout    = 1 * time.Second
+)
+
+func GetDefaultOption() *arm.ClientOptions {
+	return &arm.ClientOptions{
+		ClientOptions: policy.ClientOptions{
+			Retry: policy.RetryOptions{
+				RetryDelay:    DefaultRetryDelay,
+				MaxRetryDelay: DefaultMaxRetryDelay,
+				MaxRetries:    DefaultMaxRetries,
+				TryTimeout:    DefaultTryTimeout,
+				StatusCodes:   retryrepectthrottled.GetRetriableStatusCode(),
+			},
+			PerRetryPolicies: []policy.Policy{
+				retryrepectthrottled.NewThrottlingPolicy(),
+			},
+			Transport: &http.Client{
+				Transport: DefaultTransport,
+			},
+			TracingProvider: tracing.NewProvider(func(name, version string) tracing.Tracer {
+				return tracing.NewTracer(NewOtlpSpan, nil)
+			}, nil),
+		},
+	}
+}
